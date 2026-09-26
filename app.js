2d4bc77ba69353601a4d0e92cfdf68d5535469e4
Amanda <lemos.ane22@gmail.com>
2026-09-26 09:45:04 -0500

    ajsute

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Olá! Estou fazendo um curso da AWS. Agora vou testar minha pipeline.'));
+app.get('/', (req, res) => res.send('Ola! Estou fazendo um curso da AWS.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
